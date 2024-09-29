bef728c9abba20d631439ff7b833f1bc57500cc7
sleepstate <sleepstate@users.noreply.github.com>
2024-09-29 11:15:02 +1000

    baselined

--- create_sprites.sh
+++ create_sprites.sh
@@ -1,157 +1,126 @@
 #!/usr/bin/env bash
 
-#
-# (C) Copyright 2017, Anthony Gaudino
-# (C) Copyright 2022, Matt Topper <matt@uberether.com> and UberEther contributors
-#
-# The following script is derivative work of Anthony's sprite generator script found at:
-# https://github.com/Piotr1215/sprites/blob/master/sprites/create_sprites.sh
-# which is licensed via the GPL. This code therefore is also licensed under the terms
-# of the GNU General Public License.
-#
-
-#
-# Batch creates sprite files for PlantUml
-#
-# Given a directory can convert all PNG files to PlantUml sprite files.
-#
-# The generated sprites files formats are based on the ones introduced in
-# PlantUml 1.2017.19.
-#
-# This script assumes plantuml is in PATH.
-#
-
+# Default parameters (modifiable)
+output_dir="./icons"
+png_dir="${output_dir}/png"
+svg_dir="${output_dir}/svg"
+dir="./"
+graylevel=16
+width=48
+height=48
+prefix="ID1"
+color="black"  # Default color for icons
+background_color="transparent"  # Background color for PNGs
+report_file="sprite_report.log"
 
 # Help usage message
-usage="Batch creates sprite files for PlantUml.
+usage="Batch creates sprite files for PlantUML.
 
 $(basename "$0") [options] prefix
 
 options:
-    -p  directory path to process  Default: ./
-    -g  sprite graylevel           Default: 16
+    -p  directory path to process (default: ./)
+    -g  sprite graylevel (default: 16)
+    -w  image width (default: 48)
+    -h  image height (default: 48)
+    -o  output directory (default: ./icons)
+    -c  icon color (default: black)
+    -b  background color (default: transparent)
+    prefix: a prefix added to the sprite name."
 
-    prefix: a prefix that is added to the sprite name"
+# Function to log progress
+log_progress() {
+    echo "$1" | tee -a "$report_file"
+}
 
-
-
-# Default arguments values
-        dir="./"  # Directory path to process     Default: ./
-  graylevel=16    # Number of grayscale colors    Default: 16
-
-      prefix=""    # Prefix for sprites names, avoids having
- prefixupper=""    # two sprites with same name on STDLIB
-
-
-
-########################################
-#
-#    Main function
-#
-########################################
+# Main function
 main () {
+    log_progress "Initializing sprite generation..."
+    
     # Get arguments
-    while getopts p:w:h:g:s option
+    while getopts p:w:h:g:o:c:b: option
     do
         case "$option" in
-            p)        dir="$OPTARG";;
-            w)      width="$OPTARG";;
-            h)     height="$OPTARG";;
-            g)  graylevel="$OPTARG";;
-            :) echo "$usage"
-               exit 1
-               ;;
-           \?) echo "$usage"
-               exit 1
-               ;;
+            p) dir="$OPTARG";;
+            w) width="$OPTARG";;
+            h) height="$OPTARG";;
+            g) graylevel="$OPTARG";;
+            o) output_dir="$OPTARG"; png_dir="${output_dir}/png"; svg_dir="${output_dir}/svg";;
+            c) color="$OPTARG";;
+            b) background_color="$OPTARG";;
+            :) echo "$usage"; exit 1;;
+           \?) echo "$usage"; exit 1;;
         esac
     done
 
-    # Get mandatory argument
-    shift $(($OPTIND-1))
-    prefix=$(     echo $1 | tr '[:upper:]' '[:lower:]')
-    prefixupper=$(echo $1 | tr '[:lower:]' '[:upper:]')
+    log_progress "Using the following configuration:"
+    log_progress "Directory: $dir"
+    log_progress "Output Directory: $output_dir"
+    log_progress "Image Width: $width, Height: $height"
+    log_progress "Graylevel: $graylevel"
+    log_progress "Color: $color"
+    log_progress "Background Color: $background_color"
+    log_progress "Prefix: $prefix"
 
-    # Check mandatory argument
-    if [ -z "$prefix" ]
-    then
-        echo "Please specify a prefix!"
-        echo "$usage"
-        exit 1
-    fi
-
-
+    # Ensure output directories exist
+    mkdir -p "$png_dir"
+    mkdir -p "$svg_dir"
+    log_progress "Created output directories."
 
     # Change dir to where images are
-    if [ ! -d "${dir}" ]
-    then
-        echo "Please specify a valid directory"
+    if [ ! -d "$dir" ]; then
+        log_progress "Error: Invalid directory specified!"
         echo "$usage"
         exit 1
     fi
 
     cd "$dir"
 
-    # Setup Index Generation
-    echo "# $dir" > index.md
-	echo "### Overview" >> index.md
-	echo "| Name  | Macro  | Image | Url |" >>index.md;
-	echo "|-------|--------|-------|-----|" >>index.md;
+    # Setup README generation in root
+    echo "# Icon Set" > ../README.md
+    echo "### Overview of Generated Icons" >> ../README.md
+    echo "| Name  | Macro  | PNG  | PUML  |" >> ../README.md
+    echo "|-------|--------|------|-------|" >> ../README.md
+    log_progress "Initialized README generation."
 
     process_png
 }
 
-
-########################################
-#
-#    Generate PlantUml sprite
-#
-########################################
+# Process PNG and generate PlantUml sprites
 process_png () {
-    for i in *.png
-    do
+    log_progress "Processing PNG files..."
+    
+    for i in *.png; do
         [ -f "$i" ] || continue
-            convert "$i" -resize 48x48 -channel RGBA -matte -colorspace gray -transparent none -average "$i"
-            mv "$i" "${i//-/_}"
+        log_progress "Processing $i..."
+        convert "$i" -resize "${width}x${height}" -background "$background_color" -fill "$color" "$png_dir/$i"
+        mv "$i" "${i//-/_}"
     done
 
-    for i in *.png
-    do
-        [ -f "$i" ] || continue
+    for i in *.png; do
+        [ -f "$png_dir/$i" ] || continue
+        log_progress "Generating PUML for $i..."
 
-            filename=$(echo $i | sed -e 's/.png$//')                        # Filename without extension
-            filenameupper=$(echo $filename | tr '[:lower:]' '[:upper:]')    # Filename without extension in uppercase
-            spritename="${prefix}_$filename"                                # Sprite name is composed by prefix_filename
-            spritenameupper="${prefixupper}_$filenameupper"                 # Sprite name in uppercase
-            spritestereo="$prefixupper $filenameupper"                      # Sprite stereotype is uppercase prefix followed by uppercase filename
-            stereowhites=$(echo $spritestereo | sed -e 's/./ /g')           # This is just whitespace to make output nicer
+        filename=$(echo "$i" | sed -e 's/.png$//')
+        filenameupper=$(echo "$filename" | tr '[:lower:]' '[:upper:]')
+        spritename="${prefix}_${filenameupper}"
+        spritestereo="$prefix $filenameupper"
+        stereowhites=$(echo "$spritestereo" | sed -e 's/./ /g')
 
-        echo "@startuml" > $filename.puml
+        # Generate .puml file
+        echo "@startuml" > "$svg_dir/$filename.puml"
+        echo -e "$(plantuml -encodesprite $graylevel $png_dir/$i | sed -e '1!b' -e 's/\$/$'${prefix}_'/')\n" >> "$svg_dir/$filename.puml"
+        echo "!define ${spritename}(_color) SPRITE_PUT($stereowhites $spritename, _color)" >> "$svg_dir/$filename.puml"
+        echo "!define ${spritename}(_alias) ENTITY(rectangle,$color,$spritename,_alias,${spritename})" >> "$svg_dir/$filename.puml"
+        echo "skinparam folderBackgroundColor<<$prefixupper $filenameupper>> White" >> "$svg_dir/$filename.puml"
+        echo "@enduml" >> "$svg_dir/$filename.puml"
 
-        echo -e "$(plantuml -encodesprite $graylevel $i | sed -e '1!b' -e 's/\$/$'${prefix}_'/')\n" >> $filename.puml
-
-        echo "!define $spritenameupper(_color)                                 SPRITE_PUT(          $stereowhites          $spritename, _color)"                 >> $filename.puml
-        echo "!define $spritenameupper(_color, _scale)                         SPRITE_PUT(          $stereowhites          $spritename, _color, _scale)"         >> $filename.puml
-
-        echo "!define $spritenameupper(_color, _scale, _alias)                 SPRITE_ENT(  _alias, $spritestereo,         $spritename, _color, _scale)"         >> $filename.puml
-        echo "!define $spritenameupper(_color, _scale, _alias, _shape)         SPRITE_ENT(  _alias, $spritestereo,         $spritename, _color, _scale, _shape)" >> $filename.puml
-        echo "!define $spritenameupper(_color, _scale, _alias, _shape, _label) SPRITE_ENT_L(_alias, $spritestereo, _label, $spritename, _color, _scale, _shape)" >> $filename.puml
-
-        echo "!define $spritenameupper(_alias)                                 ENTITY(rectangle,black,$spritename,_alias,$spritenameupper)" >> $filename.puml
-        echo "!define $spritenameupper(_alias, _label)                         ENTITY(rectangle,black,$spritename,_label, _alias,$spritenameupper)" >> $filename.puml
-        echo "!define $spritenameupper(_alias, _label, _shape)                 ENTITY(_shape,black,$spritename,_label, _alias,$spritenameupper)" >> $filename.puml
-        echo "!define $spritenameupper(_alias, _label, _shape, _color)         ENTITY(_shape,_color,$spritename,_label, _alias,$spritenameupper" >> $filename.puml
-
-        echo "skinparam folderBackgroundColor<<$prefixupper $filenameupper>> White"                                                                              >> $filename.puml
-
-        echo "@enduml" >> $filename.puml
-
-
-        # Populate the index.md
-        echo "| $filename | ${prefixupper}_$filenameupper | ![image-$filename]($filename.png) |$filename.puml |" >> index.md
-
+        # Update the README with PNG and PUML links
+        echo "| $filename | ${spritename} | ![image-$filename](./icons/png/$filename.png) | $filename.puml |" >> ../README.md
+        log_progress "Added $filename to README."
     done
+    
+    log_progress "PNG and PUML processing complete."
 }
-
 
 main "$@"
